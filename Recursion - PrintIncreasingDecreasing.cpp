11adf04079515e9a0eb6f35de01e77a10a8f21d2
Raj Srinivas Jena <44380810+RajSrinivasJena@users.noreply.github.com>
2021-03-31 21:47:21 +0530

    Create Recursion - PrintIncreasingDecreasing.cpp

---
+++ Recursion - PrintIncreasingDecreasing.cpp
@@ -0,0 +1,26 @@
+
+#include<iostream>
+using namespace std;
+
+void printIncDec(int n){
+    // write your c
+    
+ if(n==0)
+  return ;
+   
+ cout<<n<<'\n';
+ printIncDec(n-1);
+ cout<<n<<'\n';
+
+}
+
+
+
+
+int main(){
+    int n; cin>>n;
+    printIncDec(n);
+    return 0;
+}
+
+ 
